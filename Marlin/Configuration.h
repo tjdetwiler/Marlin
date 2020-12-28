c048d67a4d1a8a65f3fb4dc5f767df2dcc8688eb
Tim Detwiler <timdetwiler@gmail.com>
2020-12-28 12:52:18 -0800

    Add soft endstop disable menu item.
    Allow disabling soft endstops to set 0,0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1132,7 +1132,7 @@
 #endif
 
 #if EITHER(MIN_SOFTWARE_ENDSTOPS, MAX_SOFTWARE_ENDSTOPS)
-  //#define SOFT_ENDSTOPS_MENU_ITEM  // Enable/Disable software endstops from the LCD
+  #define SOFT_ENDSTOPS_MENU_ITEM  // Enable/Disable software endstops from the LCD
 #endif
 
 /**
